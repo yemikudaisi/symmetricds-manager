15964b1d0b3a1427cbd7b568645103969ce40368
Yemi Kudaisi <yemikudaisi@gmail.com>
2021-04-01 11:52:17 +0100

    added build channel SQL capability

--- TestSQLGenerator.py
+++ TestSQLGenerator.py
@@ -4,6 +4,10 @@ from core import GroupLinkOptions
 
 class TestSQLGenerator(unittest.TestCase):
 
+    def test_create_channel(self):
+        self.assertEqual(sql_generator.create_channel('item', 1, 100000, 1, 'Item and pricing data'), \
+            "insert into sym_channel (channel_id, processing_order, max_batch_size, enabled, description) values ('item', 1, 100000, 1, 'Item and pricing data');")
+
     def test_generate_node_gp_sql(self):
         self.assertEqual(sql_generator.create_node_group('store','A retail store node'), \
             'insert into SYM_NODE_GROUP (node_group_id, description) values (\'store\', \'A retail store node\');')
--- core/sql_generator.py
+++ core/sql_generator.py
@@ -3,19 +3,15 @@ from core import GroupLinkOptions
 # Note: Generally avoided multiline string to ensure consistency
 # in built SQL thus enabling unit tests.
 
-def create_node():
-    # TODO
-    pass
+
+def create_channel(channel_id, processing_order=1, max_batch_size=100000, enabled=1, description="") -> str:
+    return f"insert into sym_channel (channel_id, processing_order, max_batch_size, enabled, description) values ('{channel_id}', {processing_order}, {max_batch_size}, {enabled}, '{description}');"
 
 def create_node_group(id: str, description: str) -> str:
     return f"insert into SYM_NODE_GROUP (node_group_id, description) values ('{id}', '{description}');"
 
 def create_node_group_link(source: str, target: str, option: GroupLinkOptions) -> str:
     return f"insert into SYM_NODE_GROUP_LINK (source_node_group_id, target_node_group_id, data_event_action) values ('{source}', '{target}', '{option.value}');"
-
-def create_channel() -> str:
-    # TODO
-    pass
 
 def create_channel_triggers(tables) -> str:
     """
