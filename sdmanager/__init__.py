08aa43d54d33fed9adb546a7870c8cbd4fea872f
Yemi Kudaisi <yemikudaisi@gmail.com>
2021-04-04 10:20:46 +0100

    updated README.md

--- sdmanager/__init__.py
+++ sdmanager/__init__.py
@@ -1,2 +1,5 @@
 import os
+  
+VERSION = "1.0.0"
+PROJECT_NAME = "symmetricds-manager"
 os.environ['SDMANAGER_BASE_DIR'] = os.path.dirname(os.path.abspath(__file__))
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from sdmanager import PROJECT_NAME, VERSION
 
 setup(
-    name='sd-manager',
-    version='0.1',
+    name=PROJECT_NAME,
+    version=VERSION,
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
